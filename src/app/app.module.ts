96b4076d0d2f9e68acd7ac0471b3d2052721548e
John Ortiz Ordoñez <John Ortiz Ordoez>
2017-03-30 23:44:37 -0500

    Section 2 | Scaffoling the Chat Application - Getting The Most Important Part Right

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,24 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ThreadSectionComponent } from './thread-section/thread-section.component';
+import { MessageSectionComponent } from './message-section/message-section.component';
+import { UserSectionComponent } from './user-section/user-section.component';
+import {ThreadsService} from "./services/threads.service";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ThreadSectionComponent,
+    MessageSectionComponent,
+    UserSectionComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [ThreadsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
